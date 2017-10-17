7f6bbe8e7f8b79fda13f240704dfa7675bfdc9a2
dach <daichi.yoshikawa1007@gmail.com>
2017-10-17 17:23:23 +0900

    Modified a spell

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,7 +29,7 @@ class PasswordResetsController < ApplicationController
     elsif @user.update_attributes(user_params)
       log_in @user
       @user.update_attribute(:reset_digest, nil)
-      flash[:success] = "Password has benn reset"
+      flash[:success] = "Password has been reset"
       redirect_to @user
     else
       render 'edit'
